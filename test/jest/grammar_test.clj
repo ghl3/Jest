186ae50b686b19bd0f9d002490afdf6ae4a58355
George Lewis <ghl227@gmail.com>
2015-01-10 19:19:07 -0500

    Clean up grammar test

--- test/jest/grammar_test.clj
+++ test/jest/grammar_test.clj
@@ -9,22 +9,3 @@
     (is (.. ast (getChild 0) getType) (. S001HelloWordParser SALUTATION))
     (is (.. ast (getChild 1) getType) (. S001HelloWordParser ENDSYMBOL))
     ))
-
-;; CommonTree leftChild = ast.getChild(0);
-;; 16
-;;   CommonTree rightChild = ast.getChild(1);
-;; 17
- 
-;; 18
-;;   //check ast structure
-;; 19
-;;   assertEquals(S001HelloWordParser.SALUTATION, leftChild.getType());
-;; 20
-;;   assertEquals(S001HelloWordParser.ENDSYMBOL, rightChild.getType());
-
-
-
-;;     (is (= ast
-;;     ))
-;; (testing "FIXME, I fail."
-;;   (is (= 0 1))))
