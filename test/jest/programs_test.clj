9687b0817fb09dbe09a40e0e4007ffdf71789542
George Lewis <ghl227@gmail.com>
2015-01-18 12:36:24 -0500

    Add list program test

--- test/jest/programs_test.clj
+++ test/jest/programs_test.clj
@@ -1,11 +1,16 @@
 (ns jest.programs-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [jest.utils :refer :all]))
 
-(import 'jest.grammar.JestParser)
-(import 'jest.grammar.JestCompiler)
+(deftest print-test
+  (let [program (slurp "resources/print.jst")]
+    (test-code
+     program
+     ["(def x 10)" "(def y 20)" "(println x)" "(println y)"])))
 
 
-(deftest func-resource-test-jst-test
-  (let [program (slurp "resources/print.jst")
-        code (. JestCompiler (getCode program))]
-    (is (= code ["(def x 10)" "(def y 20)" "(println x)" "(println y)"]))))
+(deftest list-test
+  (let [program (slurp "resources/list.jst")]
+    (test-code
+     program
+     ["(def list [1, 2, 3])" "(println list)" "(println (first list))"])))
