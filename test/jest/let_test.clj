da453153167011f7b36f00cbb6d73c49a057db71
George Lewis <ghl227@gmail.com>
2015-01-24 10:43:49 -0500

    Update let statement test

--- test/jest/let_test.clj
+++ test/jest/let_test.clj
@@ -7,3 +7,9 @@
    "let (val x=10) { x+5 };"
    ["(let [x 10] (+ x 5))"]
    15))
+
+(deftest let-test-2
+  (test-code-eval
+   "let (val x=10; val y=20) { x+y };"
+   ["(let [x 10 y 20] (+ x y))"]
+   30))
