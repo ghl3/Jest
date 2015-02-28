f2775dce668964c55310042fd16ab0aca2f0bfbe
George Lewis <ghl227@gmail.com>
2015-02-28 11:12:46 -0500

    Add pipeline test

---
+++ test/jest/pipeline_test.clj
@@ -0,0 +1,31 @@
+(ns jest.pipeline-test
+  (:require [clojure.test :refer :all]
+            [jest.utils :refer :all]))
+
+
+(deftest pipeline-test-1
+  (test-code
+   "range(0, 100)
+     ->filter(even?)
+     ->map( #(%+%) )
+     ->take(20);"
+   ["(take 20 (map #(+ % %) (filter even? (range 0 100))))"]))
+
+
+(deftest pipeline-test-2
+  (test-code-eval
+   "range(0, 100)
+     ->filter(even?)
+     ->map( #(%+%) )
+     ->take(20);"
+   ["(take 20 (map #(+ % %) (filter even? (range 0 100))))"]
+   '(0 4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76)))
+
+;(deftest pipeline-test-1
+;  (test-code-eval
+;   "range(0, 100)
+;     ->filter(even?)
+;     ->map( #(%+%) )
+;     ->take(20);"
+;   ["(take 20 (map #(+ % %) (filter even? (range 0 100))))"]
+;   (0 4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76)))
