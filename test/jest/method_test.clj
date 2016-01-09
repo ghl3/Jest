58cac8f4823b8cdea5dc79911891cbb1788ad1d3
George Lewis <ghl227@gmail.com>
2016-01-09 11:51:11 -0500

    Update method and parse tests

--- test/jest/method_test.clj
+++ test/jest/method_test.clj
@@ -5,77 +5,76 @@
 (deftest method-test-0
   (test-code
    "def g = a.foobar();"
-   ["(def g (foobar a))"]))
+   ['(def g (foobar a))]))
 
 (deftest method-test-1
   (test-code
    "def g = a.foobar(b);"
-   ["(def g (foobar a b))"]))
+   ['(def g (foobar a b))]))
 
 (deftest method-test-2
   (test-code
    "def g = x.foobar(a,b);"
-   ["(def g (foobar x a b))"]))
+   ['(def g (foobar x a b))]))
 
 (deftest method-test-3
   (test-code
    "def g = y.foobar(a, b, c);"
-   ["(def g (foobar y a b c))"]))
+   ['(def g (foobar y a b c))]))
 
 (deftest method-chain-test-1
   (test-code
    "y.foobar().bar();"
-   ["(bar (foobar y))"]))
+   ['(bar (foobar y))]))
 
 (deftest method-chain-test-2
   (test-code
    "y.foobar().bar().baz();"
-   ["(baz (bar (foobar y)))"]))
+   ['(baz (bar (foobar y)))]))
 
 (deftest method-chain-test-3
   (test-code
    "y.foobar().bar(10).baz();"
-   ["(baz (bar (foobar y) 10))"]))
+   ['(baz (bar (foobar y) 10))]))
 
 (deftest method-chain-test-4
   (test-code
    "y.foobar(2).bar(10).baz();"
-   ["(baz (bar (foobar y 2) 10))"]))
+   ['(baz (bar (foobar y 2) 10))]))
 
 (deftest method-chain-test-5
   (test-code
    "y.foobar(2).bar(10, 20, 15).baz(true);"
-   ["(baz (bar (foobar y 2) 10 20 15) true)"]))
+   ['(baz (bar (foobar y 2) 10 20 15) true)]))
 
 
  (deftest method-chain-test-6
    (test-code
     "range(0, 100).first();"
-    ["(first (range 0 100))"]))
+    ['(first (range 0 100))]))
 
 
  (deftest method-chain-test-7
    (test-code
     "[1, 2, 3].get(1);"
-    ["(get [1, 2, 3] 1)"]))
+    ['(get [1, 2, 3] 1)]))
 
 
  (deftest method-pipe-test-1
    (test-code
     "y->filter(even?);"
-    ["(filter even? y)"]))
+    ['(filter even? y)]))
 
 
  (deftest method-pipe-test-2
    (test-code
     "range(0, 10)->filter(even?)->apply(sum);"
-    ["(apply sum (filter even? (range 0 10)))"]))
-
+    ['(apply sum (filter even? (range 0 10)))]))
 
 
  (deftest method-mixed-test-1
    (test-code
     "range(0, 10)->filter(even?).get(0);"
-    ["(get (filter even? (range 0 10)) 0)"]))
+    ['(get (filter even? (range 0 10)) 0)]))
 
 
--- test/jest/parse_test.clj
+++ test/jest/parse_test.clj
@@ -5,35 +5,34 @@
 (deftest val-test
   (test-code
    "def foo = 10;"
-   ["(def foo 10)"]))
+   ['(def foo 10)]))
 
 (deftest multi-val-test
   (test-code
    "def foo = 10; \n def bar = 20;"
-   ["(def foo 10)" "(def bar 20)"]))
+   ['(def foo 10) '(def bar 20)]))
 
 (deftest println-test
   (test-code
    "println(a);"
-   ["(println a)"]))
+   ['(println a)]))
 
 (deftest func-def-test
   (test-code
    "defn foobar(a, b, c){ 10; };"
-   ["(defn foobar [ a b c ] 10)"]))
+   ['(clojure.core/defn foobar [ a b c ] 10)]))
 
 (deftest import-test
   (test-code
    "import foo.bar;"
-   ["(import 'foo.bar)"]))
+   ['(clojure.core/import foo.bar)]))
 
 (deftest sum-test
   (test-code
    "def list = [1, 2, 3];\nprintln(sum(list));"
-   ["(def list [1, 2, 3])" "(println (sum list))"]))
-
+   ['(def list [1, 2, 3]) '(println (sum list))]))
 
 (deftest symbol-test
   (test-code
    "def x = :mySymbol;"
-   ["(def x :mySymbol)"]))
+   ['(def x :mySymbol)]))
