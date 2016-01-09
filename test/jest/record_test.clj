4fb5a3e0b58e65e8d557f8b4f22b2788a3974c26
George Lewis <ghl227@gmail.com>
2016-01-09 12:22:14 -0500

    Update record test

--- test/jest/record_test.clj
+++ test/jest/record_test.clj
@@ -5,7 +5,7 @@
 (deftest record-test-1
     (test-code
      "record Student{ name; class; }"
-     ["(defrecord Student [name class])"]))
+     ['(clojure.core/defrecord Student [name class])]))
 
 
 (deftest record-test-2
@@ -13,9 +13,9 @@
      "record Student{ name; class; }
       def bob = new Student(\"Bob\", \"History\");
       bob.name;"
-     ["(defrecord Student [name class])"
-      "(def bob (->Student \"Bob\" \"History\"))"
-      "(:name bob)"]
+     ['(clojure.core/defrecord Student [name class])
+      '(def bob (->Student "Bob" "History"))
+      '(:name bob)]
      "Bob"))
 
 (deftest record-test-3
@@ -25,21 +25,21 @@
       def history = new CollegeClass(\"Noon\", \"Wednesday\");
       def bob = new Student(\"Bob\", history);
       bob.class.time;"
-     ["(defrecord Student [name class])"
-      "(defrecord CollegeClass [time day])"
-      "(def history (->CollegeClass \"Noon\" \"Wednesday\"))"
-      "(def bob (->Student \"Bob\" history))"
-      "(:time (:class bob))"]
+     ['(clojure.core/defrecord Student [name class])
+      '(clojure.core/defrecord CollegeClass [time day])
+      '(def history (->CollegeClass "Noon" "Wednesday"))
+      '(def bob (->Student "Bob" history))
+      '(:time (:class bob))]
      "Noon"))
 
 (deftest record-test-4
-    (test-code-eval
+    (test-code-eval (str
      "record Student{ name; class; }
       def bob = new Student(name: \"Bob\", class: \"History\");
-      bob.name;"
-     ["(defrecord Student [name class])"
-      "(def bob (map->Student {:name \"Bob\" :class \"History\"}))"
-      "(:name bob)"]
+      bob.name;")
+     ['(clojure.core/defrecord Student [name class])
+      '(def bob (map->Student {:name "Bob" :class "History"}))
+      '(:name bob)]
      "Bob"))
 
 
@@ -55,6 +55,6 @@
            }
          }
      }"
-     ["(defrecord Student [name class]
+     ['(clojure.core/defrecord Student [name class]
         foo
-          (sit [ this ] (println this)))"]))
+          (sit [ this ] (println this)))]))
